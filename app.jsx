61664805df2bc4d34b66872f1b93ac15bfb46e97
Abhik004 <abhiksamanta004@gmail.com>
2024-06-18 20:40:19 +0530

    HomePage

--- app.jsx
+++ app.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import HomePage from './components/Homepage';
+import './App.css';
 
 let root = ReactDOM.createRoot(document.querySelector("#root"));
 
 const Container = () => {
     return (
       <div id="container">
-        <Homepage/>
+        <HomePage/>
         <AboutUs/>
         <ElectionNews />
         <Rules/>
